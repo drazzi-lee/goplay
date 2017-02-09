a354e435197fc73479c6aa6e1d9d64a06f0a9466
Li Pengfei <lipengfei05@baidu.com>
2017-02-09 20:02:28 +0800

    add composite type example

--- hello.go
+++ hello.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"golang.org/x/tour/pic"
+	"golang.org/x/tour/wc"
 )
 
 func Pic(dx, dy int) [][]uint8 {
@@ -17,9 +19,52 @@ func Pic(dx, dy int) [][]uint8 {
     return a
 }
 
+func WordCount(s string) map[string]int {
+    s_arr := strings.Fields(s)
+    s_map := make(map[string]int)
+    for i := 0; i < len(s_arr); i++ {
+        if s_map[s_arr[i]] == 0 {
+            s_map[s_arr[i]] = 1
+        } else {
+            s_map[s_arr[i]]++
+        }
+    }
+    return s_map
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func fibonacci() func() int {
+	i, j := 0, 1
+	return func() int {
+		i, j = j, i + j
+		return i
+	}
+}
+
 func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Println("Hello, 世界")
 	fmt.Printf("Hello, %s, %d\n", "Drazzi", 291)
 	pic.Show(Pic)
+
+	wc.Test(WordCount)
+
+	//closure
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(pos(i), neg(-2 * i))
+	}
+
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		//fmt.Println(fibonacci())
+		fmt.Println(f())
+	}
 }
