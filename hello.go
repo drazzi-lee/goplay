f4de4b0b8ab890c99433fcad46f3c74b9a166e2c
Li Pengfei <lipengfei05@baidu.com>
2017-02-09 19:24:25 +0800

    interesting image

--- hello.go
+++ hello.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+    a := make([][]uint8, dy)
+    for x := range a {
+        b := make([]uint8, dx)
+        for y:= range b {
+            b[y] = uint8(2*x)
+        }
+        a[x] = b
+    }
+    return a
+}
 
 func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Println("Hello, 世界")
 	fmt.Printf("Hello, %s, %d\n", "Drazzi", 291)
+	pic.Show(Pic)
 }
